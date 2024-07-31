8b374b13c904adf0397904ea0a252984d528ae3d
Swathi Anusha .Ummidi <147793429+swathianusha@users.noreply.github.com>
2024-07-31 15:38:21 +0530

    import java.util.Scanner; public class Division {     public static void main (String[] args) {         int a,b,c;         Scanner read = new Scanner(System.in);         a = read.nextInt();         b = read.nextInt();         c = a / b;         System.out.println(c);
        }
    }

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Division {
+    public static void main (String[] args) {
+        int a,b,c;
+        Scanner read = new Scanner(System.in);
+        a = read.nextInt();
+        b = read.nextInt();
+        c = a / b;
+        System.out.println(c);
+        
+    }
+}
